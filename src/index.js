4049eb0039e7d90f10c6535d60a8005b5c6fc616
ason0008 <ason0008@student.monash.edu>
2020-10-20 16:25:30 +0800

    update firebase issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from './App';
 import './index.css';
 import './fonts/Overpass/Overpass-Regular.ttf'
 
+if('serviceWorker' in navigator) { 
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+      messaging.useServiceWorker(registration);  
+    }); 
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
