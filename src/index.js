84bd76ac0eff056acc803553bbc53e199a646194
ason0008 <ason0008@student.monash.edu>
2020-10-20 18:43:57 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,6 @@ import App from './App';
 import './index.css';
 import './fonts/Overpass/Overpass-Regular.ttf'
 
-import { messaging } from "./Firebase";
-
-if('serviceWorker' in navigator) { 
-  navigator.serviceWorker.register('../public/firebase-messaging-sw.js')
-    .then(function(registration) {
-      console.log("Service Worker Registered");
-      messaging.useServiceWorker(registration);  
-    }); 
-}
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
