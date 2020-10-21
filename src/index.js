e256df69a54ccc4a81176bd05405be4d32d6282c
ason0008 <ason0008@student.monash.edu>
2020-10-21 14:52:03 +0800

    update service worker path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import './fonts/Overpass/Overpass-Regular.ttf'
+import './fonts/Overpass/Overpass-Regular.ttf';
+
+import { messaging } from './Firebase';
+
+if('serviceWorker' in navigator) { 
+  navigator.serviceWorker.register('/IPS/firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+      messaging.useServiceWorker(registration);  
+    }); 
+}
 
 ReactDOM.render(
   <App />,
